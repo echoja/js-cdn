d029b85d34ab96f2a8e318c26c8702046b83e2d0
김태훈 <73801151+echoja@users.noreply.github.com>
2022-05-06 23:43:19 +0900

    fix cookie-test

--- cookie-test.js
+++ cookie-test.js
@@ -1,3 +1,3 @@
-import Cookies from "https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.min.js";
+import Cookies from "https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.mjs";
 
 Cookies.set("foo", "bar");
